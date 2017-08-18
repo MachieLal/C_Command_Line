f823790492a789a07f9bcc06f453c2ab8d044e98
SwarupPattnaik <swarup.pattnaik@evry.com>
2017-08-18 12:57:55 +0530

    Initial Commit

---
+++ C_Command_Line/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_Command_Line
+//
+//  Created by Swarup on 8/18/17.
+//  Copyright © 2017 Swarup. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
